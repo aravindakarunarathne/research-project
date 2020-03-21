511d5957ff388f0bb00578363d6f4ad1083fc272
aravinda <aravinda.srimal@gmail.com>
2020-03-21 22:41:35 +0530

    registration frontend-1

--- registration.php
+++ registration.php
@@ -1,27 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Registrations</title>
 </head>
+
 <body>
-   <div class="container">
-       <div class="header">
-           <h2>Register</h2>
-       </div>
-       
-       <form action="registration.php" method="post">
-           
-           <div>
-               <label>username</label>
-               <input type ="text" name="username">
-           </div>
-           
-           
-       </form>
-   </div>
-    
+    <div class="container">
+        <div class="header">
+            <h2>Register</h2>
+        </div>
+
+        <form action="registration.php" method="post">
+
+            <div>
+                <label>username</label>
+                <input type="text" name="username" placeholder="Enter your username">
+            </div>
+            <div>
+                <label>email</label>
+                <input type="text" name="email" placeholder="Enter your email">
+            </div>
+
+            <div>
+                <label>password</label>
+                <input type="password" name="password" placeholder="Enter your password">
+            </div>
+
+            <div>
+                <label>confirm password</label>
+                <input type="password" name="confirm password" placeholder="Type password again">
+            </div>
+
+
+
+        </form>
+    </div>
+
 </body>
+
 </html>
 
 <?php
