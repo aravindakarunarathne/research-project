8ffb34bee271a5c55a9527a770b762f6eff4c6c9
aravinda <aravinda.srimal@gmail.com>
2020-03-21 22:25:06 +0530

    registration

---
+++ registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registrations</title>
+</head>
+<body>
+   <div class="container">
+       <div class="header">
+           <h2>Register</h2>
+       </div>
+       
+       <form action="registration.php" method="post">
+           
+           <div>
+               <label>username</label>
+               <input type ="text" name="username">
+           </div>
+           
+           
+       </form>
+   </div>
+    
+</body>
+</html>
+
+<?php
+/*username
+email
+password
+confirm password */
+    ?>
